bd235c7fbf7fbf73fdca3e24f0989f19f0ff7ca3
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2025-04-10 16:07:46 +0530

    Corrected ContactPage

--- frontend/src/components/Sub/Form.tsx
+++ frontend/src/components/Sub/Form.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import FormTextAreaField from "./FormTextArea";
+import FormField from "./FormField";
 
 export default function Form() {
   const [entityName, setEntityName] = useState("");
@@ -223,67 +225,3 @@ export default function Form() {
     </section>
   );
 }
-
-const FormField = ({
-  label,
-  id,
-  type = "text",
-  value,
-  onChange,
-  placeholder,
-}: {
-  label: string;
-  id: string;
-  type?: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  placeholder: string;
-}) => {
-  return (
-    <div
-      id="contact"
-      className="flex flex-col gap-2 sans max-w-full min-h-[88px]"
-    >
-      <label htmlFor={id} className="sm:text-2xl font-bold">
-        {label}
-      </label>
-      <input
-        id={id}
-        type={type}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        className="border-2 border-[#979797] max-w-[70vw] text-[#444444] font-normal text-[8px] sm:text-sm md:text-[16px] boxShadow rounded-xl px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-    </div>
-  );
-};
-
-const FormTextAreaField = ({
-  label,
-  id,
-  value,
-  onChange,
-  placeholder,
-}: {
-  label: string;
-  id: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  placeholder: string;
-}) => {
-  return (
-    <div className="flex flex-col gap-2 sans min-h-[176px]">
-      <label htmlFor={id} className="text-2xl font-bold">
-        {label}
-      </label>
-      <textarea
-        id={id}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        className="border-2 border-[#979797] max-w-[70vw] h-full mb-2 text-[#444444] font-normal text-sm md:text-[16px] boxShadow rounded-xl px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-    </div>
-  );
-};

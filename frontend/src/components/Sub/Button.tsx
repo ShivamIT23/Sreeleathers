734a9025a852afbe183d2c5a61a40cf5c359d727
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2025-04-14 11:24:36 +0530

    Changed a few things 2

--- frontend/src/components/Sub/Button.tsx
+++ frontend/src/components/Sub/Button.tsx
@@ -6,26 +6,13 @@ export default function Button({ children }: { children: string }) {
     }
   };
   return (
-    <>
-      {children == "Submit" ? (
-        <div
-          onClick={() => scrollToSection("franchise")}
-          className="rounded-[100px] w-fit min-w-32 bg-[linear-gradient(to_bottom,_#03A3D3,_#0A92C1)] p-4 gap-2 flex md:whitespace-nowrap shadow transition"
-        >
-          <button className="bg-inherit text-white w-fit h-fit roboto font-medium md:text-[16px]">
-            {children}
-          </button>
-        </div>
-      ) : (
-        <div
-          onClick={() => scrollToSection("franchise")}
-          className="rounded-[100px] w-fit min-w-32 bg-[linear-gradient(to_bottom,_#03A3D3,_#0A92C1)] p-4 gap-2 flex md:whitespace-nowrap shadow transition"
-        >
-          <button className="bg-inherit text-white w-fit h-fit roboto font-medium md:text-[16px]">
-            {children}
-          </button>
-        </div>
-      )}
-    </>
+    <div
+      onClick={() => scrollToSection("franchise")}
+      className="rounded-[100px] w-fit min-w-32 bg-[linear-gradient(to_bottom,_#03A3D3,_#0A92C1)] p-4 gap-2 flex md:whitespace-nowrap shadow transition"
+    >
+      <button className="bg-inherit text-white w-fit h-fit roboto font-medium md:text-[16px]">
+        {children}
+      </button>
+    </div>
   );
 }
